d584c1524a83332d49d6dba62e651abb0df93cca
Oathian <aidanmaycock@gmail.com>
2022-05-17 14:03:33 +0100

    err handlers reordered

--- app.js
+++ app.js
@@ -17,18 +17,20 @@ app.all("/*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    if(err.status && err.msg) {
-        res.status(err.status).send({ msg: err.msg })
+    if(err.code === "22P02") {
+        res.status(400).send({ msg: "Invalid input" });
     } else {
         next(err);
     }
 });
 
 app.use((err, req, res, next) => {
-    if(err.code === "22P02") {
-        res.status(400).send({ msg: "Invalid input" });
+    if(err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg })
+    } else {
+        next(err);
     }
-})
+});
 
 app.use((err, req, res, next) => {
     console.log(err, "<<< uncaught error");
