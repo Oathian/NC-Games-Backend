1d1374c3dccc76f1d402ae3aeef81c6f569543df
Oathian <aidanmaycock@gmail.com>
2022-07-02 22:27:51 +0100

    comment votes added

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,13 +1,30 @@
-const { removeCommentById } = require("../models/comments.models.js");
+const { removeCommentById, patchCommentVotes } = require("../models/comments.models.js");
 
 exports.deleteCommentById = (req, res, next) => {
 
     const { comment_id } = req.params;
 
     removeCommentById(comment_id).then(() => {
+
         res.status(204).end();
+
     }).catch((err) => {
         next(err);
     })
 
+}
+
+exports.addCommentVotes = (req, res, next) => {
+
+    const { inc_votes } = req.body;
+
+    const { comment_id } = req.params;
+
+    patchCommentVotes(comment_id, inc_votes).then((newComment) => {
+
+        res.status(200).send({ newComment });
+
+    }).catch((err) => {
+        next(err);
+    })
 }
